375024a7b5ddc54f79cf762e85753e2e381a2337
Ventsislav <v.mladenov@hotmail.com>
2019-08-02 12:15:03 +0200

    Update take tests, to use array and iterable

--- test/unit/take.spec.js
+++ test/unit/take.spec.js
@@ -1,37 +1,65 @@
 import { expect } from 'chai';
-import { fromIterable } from "../../src";
+import { fromIterable, range } from "../../src";
 
 describe('take tests', () => {
     it('should take first n numbers', () => {
-        const output = fromIterable([1, 2, 3, 4, 5]).take(3).toArray();
-        expect(output).to.deep.equal([1, 2, 3]);
+        const output0 = fromIterable([1, 2, 3, 4, 5]).take(3).toArray();
+        expect(output0).to.deep.equal([1, 2, 3]);
+
+        // use iterable
+        const output1 = range(1, 6).take(3).toArray();
+        expect(output1).to.deep.equal([1, 2, 3]);
     });
 
     it('should take first n numbers after another operation', () => {
-        const output = fromIterable([1, 2, 3, 4, 5]).where(_ => _ > 2).take(3).toArray();
-        expect(output).to.deep.equal([3, 4, 5]);
+        const output0 = fromIterable([1, 2, 3, 4, 5]).where(_ => _ > 2).take(3).toArray();
+        expect(output0).to.deep.equal([3, 4, 5]);
+
+        // use iterable
+        const output1 = range(1, 6).where(_ => _ > 2).take(3).toArray();
+        expect(output1).to.deep.equal([3, 4, 5]);
     });
 
     it('should take be able to continue with another operator', () => {
-        const output = fromIterable([1, 2, 3, 4, 5])
+        const output0 = fromIterable([1, 2, 3, 4, 5])
             .where(_ => _ > 2)
             .take(3)
             .select(x => `item_${x}`)
             .toArray();
-        expect(output).to.deep.equal(['item_3', 'item_4', 'item_5']);
+        expect(output0).to.deep.equal(['item_3', 'item_4', 'item_5']);
+
+        // use iterable
+        const output1 = range(1, 6)
+            .where(_ => _ > 2)
+            .take(3)
+            .select(x => `item_${x}`)
+            .toArray();
+        expect(output1).to.deep.equal(['item_3', 'item_4', 'item_5']);
     });
 
     it('should take all elements if source count is less', () => {
-        const output = fromIterable([1, 2])
+        // use array
+        const output0 = fromIterable([1, 2])
             .take(3)
             .toArray();
-        expect(output).to.deep.equal([1, 2]);
+        expect(output0).to.deep.equal([1, 2]);
+
+        // use iterable
+        const output1 = range(1, 3)
+            .take(3)
+            .toArray();
+        expect(output1).to.deep.equal([1, 2]);
     });
 
     it('should take nothing from empty collection', () => {
-        const output = fromIterable([])
+        const output0 = fromIterable([])
             .take(1)
             .toArray();
-        expect(output).to.deep.equal([]);
+        expect(output0).to.deep.equal([]);
+
+        const output1 = range(1, 1)
+            .take(1)
+            .toArray();
+        expect(output1).to.deep.equal([]);
     });
 });
