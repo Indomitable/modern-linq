8162e5d60c9ecc556d0b64da03566c7149a03a7a
Ventsislav <v.mladenov@hotmail.com>
2019-08-07 11:25:21 +0200

    for-each testing: check forEach after where operator.

--- test/unit/for-each.spec.js
+++ test/unit/for-each.spec.js
@@ -27,4 +27,26 @@ describe('for each tests', () => {
     });
 
 
+    [
+        new Set([
+            new Person(10, 'A'),
+            new Person(20, 'B'),
+            new Person(30, 'C'),
+        ]),
+        [
+            new Person(10, 'A'),
+            new Person(20, 'B'),
+            new Person(30, 'C'),
+        ]
+    ].forEach((source, indx) => {
+        it('should do for each items in sequence after other functions: ' + indx, () => {
+            fromIterable(source).where(p => p.age > 10).forEach(p => {
+                p.age = p.age * 2;
+            });
+            expect(fromIterable(source).elementAt(0).age).to.equal(10);
+            expect(fromIterable(source).elementAt(1).age).to.equal(40);
+            expect(fromIterable(source).elementAt(2).age).to.equal(60);
+        });
+    });
+
 });
